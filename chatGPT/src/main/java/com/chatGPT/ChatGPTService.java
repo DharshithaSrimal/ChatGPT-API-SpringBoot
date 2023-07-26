d82e030b959dce76f7e215d9424e8704d75cecd5
Dharshitha Senevirathne <dharshithasrimal@gmail.com>
2023-07-26 23:57:28 +0530

    Create ChatGPTService

---
+++ chatGPT/src/main/java/com/chatGPT/ChatGPTService.java
@@ -0,0 +1,10 @@
+package com.chatGPT;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ChatGPTService {
+    public String processSearch(String query){
+        return "";
+    }
+}
