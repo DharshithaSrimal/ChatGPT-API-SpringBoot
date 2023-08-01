5907fa40f6230bbcb309378a282670d4292022e0
Dharshitha Senevirathne <dharshithasrimal@gmail.com>
2023-08-02 00:25:10 +0530

    Response formatting

--- chatGPT/src/main/java/com/chatGPT/model/ChatGPTRequest.java
+++ chatGPT/src/main/java/com/chatGPT/model/ChatGPTRequest.java
@@ -1,6 +1,6 @@
 package com.chatGPT.model;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.gson.annotations.SerializedName;
 import lombok.Data;
 
 @Data
@@ -8,6 +8,6 @@ public class ChatGPTRequest {
     private String model = "text-davinci-003";
     private String prompt;
     private int temperature = 1;
-    @JsonProperty(value = "max_tokens")
+    @SerializedName(value = "max_tokens")
     private int maxTokens = 100;
 }
